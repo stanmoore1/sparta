5da5ebb6ec67a4a8a826dada2f135bb4a4e2773c
Stan Gerald Moore <stamoor@sandia.gov>
2023-09-15 08:04:24 -0600

    Update Kokkos GPU CMake preset files

--- cmake/presets/kokkos_hip.cmake
+++ cmake/presets/kokkos_hip.cmake
@@ -1,4 +1,4 @@
-# kokkos_cuda = KOKKOS package with CUDA backend, default MPI, nvcc/mpicxx
+# kokkos_hip = KOKKOS package with HIP backend, default MPI, hipcc
 # compiler with OpenMPI or MPICH
 
 include(${CMAKE_CURRENT_LIST_DIR}/kokkos_common.cmake)
@@ -19,10 +19,10 @@ set(CMAKE_CXX_COMPILER
 # ################### END CMAKE OPTIONS ####################
 
 # ################### BEGIN KOKKOS OPTIONS ####################
-set(Kokkos_ENABLE_CUDA
+set(Kokkos_ENABLE_HIP
     ON
     CACHE STRING "")
-set(Kokkos_ARCH_VOLTA70
+set(Kokkos_ARCH_VEGA90A
     ON
     CACHE STRING "")
 # ################### END   KOKKOS OPTIONS ####################
